0a334c030bb2e432569ccfae7367679da18f8f1e
Tyler-Will <tylerceeme@gmail.com>
2023-04-04 18:37:07 -0700

    Started routes

---
+++ Backend/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: "zalatar5689",
+    host: "localhost",
+    port: 5432,
+    database: "Budget"
+})
+
+module.exports = pool;
---
+++ Backend/index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const app= express();
+const cors = require("cors");
+const pool = require("./db")
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log("server has started on port 5000");
+});
+
+//ROUTES//
+
+//Show Budget
+/*app.get("/Budget", async(req, res) => {
+    try {
+        const yourBudget = await pool.query("SELECT * FROM your_budget");
+        res.json(yourBudget.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+//Update Budget
+/*app.put("/Budget/:budget_id", async (req, res) => {
+    try {
+        const {budget_id} = req.params;
+        const {current_budget} = req.body;
+        const updateBudget = await pool.query("UPDATE your_budget SET current_budget = $1 WHERE current_budget = $2", [current_budget, budget_id]
+        );
+
+        res.json("Budget updated!");
+    }catch (err) {
+        console.error(err.message)
+    }
+})*/
+//Add expense
+app.post("/Budget", async(req, res) => {
+    try {
+        const {expense, expense_cost} = req.body;
+        const newExpense = await pool.query("INSERT INTO expenses (expense, expense_cost) VALUES($1, $2)", 
+        [expense, expense_cost]
+        );
+        res.json(newExpense.rows[0])
+    } catch (err) {
+        console.error(err.message);
+    }
+    });
+
+//Show all expenses
+app.get("/Budget", async(req, res) => {
+    try {
+        const allExpenses = await pool.query("SELECT * FROM expenses");
+        res.json(allExpenses.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+
+//Show specific expense (Might not use)
+app.get("/Budget/:expense", async (req, res) => {
+    try{
+        const {expense} = req.params;
+        const expenses = await pool.query("SELECT * FROM expenses WHERE expense = $1", [expense])
+
+        res.json(expenses.rows[0])
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//Update expense Cost
+app.put("/Budget/:expense", async (req, res) => {
+    try {
+        const {expense} = req.params;
+        const {expense_cost} = req.body;
+        const updateExpense = await pool.query("UPDATE expenses SET expense_cost = $1 WHERE expense = $2", [expense_cost, expense]
+        );
+
+        res.json("Cost updated!");
+    }catch (err) {
+        console.error(err.message)
+    }
+})
+
+//Delete expense
+app.delete("/Budget/:expense", async (req, res) => {
+    try {
+        const {expense} = req.params;
+        const deleteExpense = await pool.query("DELETE FROM expenses WHERE expense = $1", [expense]
+        );
+        res.json("You deleted the expense!");
+    }catch (err) {
+        console.error(err.message)
+    }
+})
